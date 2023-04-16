60328af388784b662877a016c0670791d039c35a
TodorGit <79662469+TodorGit@users.noreply.github.com>
2023-04-16 22:43:26 +0100

    React App Design part

---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './whatGPT3.css';
+import { Feature } from '../../components';
+
+const WhatGPT3 = () => {
+  return (
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        < Feature title="What is GPT-3" text = "We so opinion frend me message as delight.Whole front do of plate whatever defctive nor convinced residence own.Connetion has put impossible restraints boisterous.At joinure ladyship an insted so jumanity he.Fridenly but in the end is covered by a different scope" />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Explor The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature title="Chatbots" text = "We so opinion friends messafe as delight.Whole front do of plate heard oh ought." />
+        <Feature title="Knowledgebase" text= "At jointure ladiship an instead so humanity he. Friendly bachelor entrance to on by.As put impossible own apartments b"/>
+        <Feature title="Education" text= "At jointure ladiship an instead so humanity he. Friendly bachelor entrance to on by.As put impossible own apartments b"/>
+      </div>
+    </div>
+  )
+}
+
+export default WhatGPT3;
